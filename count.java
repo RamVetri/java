12b7ddec5fc2d14fc1a7859d30a5e567566e03eb
RamVetri <116931679+RamVetri@users.noreply.github.com>
2022-10-29 05:29:08 -0700

    Add files via upload

---
+++ count.java
@@ -0,0 +1,12 @@
+public class count {
+    public static void main(String[] args) {
+        int []arr={40,30,10,40,20,30};
+        int len=arr.length;
+        System.out.println(len);
+        System.out.println(arr[]);
+        for(int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++){
+            }
+        }
+    }
+}
